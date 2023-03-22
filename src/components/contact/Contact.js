fe20fff1457c1326a9b474e09ce965326bdf599d
Joel Smith <joelsmith.2019@gmail.com>
2023-03-21 17:51:49 -0700

    Fix contact box spacing

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,49 +2,54 @@ import './Contact.scss';
 
 function Contact() {
     return (
-        <div id="contact" className="container-sm">
+        <div id="contact" className="container-sm" style={{ overflow: 'hidden' }}>
+
+            <div className="mx-3">
 
                 {/* Contact Me */}
-                <div className="row mx-3">
-                    <h1>Contact</h1>
+                <div className="row">
+                    <div className="col text-center">
+                        <h1>Contact Me</h1>
+                    </div>
                 </div>
 
                 {/* Social Links */}
-                <div className="row mx-3 w-100">
+                <div className="row justify-content-center align-items-center">
 
-
-                    <div className="row justify-content-between">
-
-                        <div className="col-12 col-sm-6">
-                            <p>
-                                Please connect with me on LinkedIn, GitHub, or send me an email.
-                            </p>
+                    {/* Contact Text */}
+                    <div className="col-7 col-sm-6">
+                        <div className="d-flex justify-content-center align-items-center">
+                            <span className="text-center text-sm-start">
+                                Connect with me on LinkedIn, GitHub, or via email.
+                            </span>
                         </div>
+                    </div>
 
-                        <div className="col-12 col-sm-6">
+                    {/* Social Icons */}
+                    <div className="col-7 col-sm-6">
+                        <div className="d-flex justify-content-center align-items-center">
 
-                            <div className="d-flex justify-content-center align-items-center">
-                                {/* Linked In Icon */}
-                                <div className="social-icon">
-                                    <a href="https://linkedin.com/in/joelsmith-2019" target="_blank">
-                                        <i className="fa-brands fa-linkedin fa-4x"></i>
-                                    </a>
-                                </div>
-
-                                {/* GitHub Icon */}
-                                <div className="social-icon mx-3">
-                                    <a href="https://github.com/joelsmith-2019" target="_blank">
-                                        <i className="fa-brands fa-github fa-4x"></i>
-                                    </a>
-                                </div>
-
-                                {/* Email Icon */}
-                                <div className="social-icon">
-                                    <a href="mailto:joelsmith.2019@gmail.com" target="_blank">
-                                        <i className="fa-solid fa-envelope fa-4x"></i>
-                                    </a>
-                                </div>
+                            {/* Linked In Icon */}
+                            <div className="social-icon">
+                                <a href="https://linkedin.com/in/joelsmith-2019" target="_blank">
+                                    <i className="fa-brands fa-linkedin fa-4x"></i>
+                                </a>
                             </div>
+
+                            {/* GitHub Icon */}
+                            <div className="social-icon mx-3">
+                                <a href="https://github.com/joelsmith-2019" target="_blank">
+                                    <i className="fa-brands fa-github fa-4x"></i>
+                                </a>
+                            </div>
+
+                            {/* Email Icon */}
+                            <div className="social-icon">
+                                <a href="mailto:joelsmith.2019@gmail.com" target="_blank">
+                                    <i className="fa-solid fa-envelope fa-4x"></i>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
