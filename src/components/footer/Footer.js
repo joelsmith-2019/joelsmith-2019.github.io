76f06663997b6aa3080046afd1eee3f22192e99c
Joel Smith <joelsmith.2019@gmail.com>
2023-03-23 19:50:43 -0700

    Add footer content

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="container-sm">
+                <div className="d-flex justify-content-center align-items-center text-center">
+                    <a href="https://github.com/joelsmith-2019/portfolio" target="_blank" rel="noopener noreferrer">
+                        Designed and Developed by Joel Smith
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
